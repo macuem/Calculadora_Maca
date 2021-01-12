e3ab87c097de205c16b84018bd02a1509714b491
macuem <77363258+macuem@users.noreply.github.com>
2021-01-12 19:38:24 -0300

    prueba

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Calculadora_Clase_2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
